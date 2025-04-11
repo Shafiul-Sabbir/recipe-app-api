d9a15ec946ed795b9b032f1b33a61bd6cf000289
Shafiul-Sabbir <sabbirvai82@gmail.com>
2025-04-11 20:31:27 +0600

    solving extra line issues

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -21,7 +21,7 @@ class Command(BaseCommand):
                 self.check(databases=['default'])
                 db_up = True
                 
-            except( Psycopg2OpError, OperationalError ):
+            except(Psycopg2OpError, OperationalError):
                 self.stdout.write('Database unavailable, waiting 1 second...')
                 time.sleep(1)
                 
