6aacdcdc572f40fc23880f77b2cc70ae66013dfc
Shafiul-Sabbir <sabbirvai82@gmail.com>
2025-04-11 20:18:32 +0600

    solving end of line issues

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -3,11 +3,11 @@ Test custom Django management commands.
 """
 from unittest.mock import patch
 
-from psycopg2 import OperationalError as Psycopg2Error
-
 from django.core.management import call_command
 from django.db.utils import OperationalError
 from django.test import SimpleTestCase
+from psycopg2 import OperationalError as Psycopg2Error
+
 
 @patch('core.management.commands.wait_for_db.Command.check')
 class CommandTests(SimpleTestCase):
@@ -31,5 +31,4 @@ class CommandTests(SimpleTestCase):
         
         self.assertEqual(patched_check.call_count, 6)
         patched_check.assert_called_with(databases=['default'])
-        
         
