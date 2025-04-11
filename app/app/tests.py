29444132569840d7f9b410cef1e7385f311d82eb
Shafiul-Sabbir <sabbirvai82@gmail.com>
2025-04-11 20:28:15 +0600

    solving extra line issues

--- app/app/tests.py
+++ app/app/tests.py
@@ -7,14 +7,14 @@ from django.test import SimpleTestCase
 
 
 class CalcTests(SimpleTestCase):
-    """ Test the calc module. """
-    
+    """Test the calc module."""
+
     def test_add_numbers(self):
-        """Test adding numbers together. """
+        """Test adding numbers together."""
         res = calc.add(5, 6)
         self.assertEqual(res, 11)
-    
+
     def test_subtract_numbers(self):
+        """Test subtracting numbers."""
         res = calc.subtract(10, 15)
         self.assertEqual(res, 5)
-        
